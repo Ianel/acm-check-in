c6c1447dec720ca4a93c05a7ca9ef1eb36991c8e
Ianel Tombozafy <ianell.s118@gmail.com>
2025-11-04 22:05:11 +0300

    initial commit

---
+++ src/layout/MainLayout.tsx
@@ -0,0 +1,15 @@
+export default function MainLayout() {
+    return (
+        <div>
+            <header>
+                <img src="/logo.png" alt="Logo ACM" />
+                <h1>
+                    <span>American Corner</span>
+                    <span>Mahajanga</span>
+                </h1>
+            </header>
+
+            <main></main>
+        </div>
+    );
+}
